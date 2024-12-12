86b88ff964d735f60ed6d2434d4224d61e2ac622
Arunapindra <150082622+Arunapindra@users.noreply.github.com>
2024-12-12 13:13:21 -0600

    python practice

--- hello.py
+++ hello.py
@@ -1,2 +1,22 @@
-print("hello world")
+# print("hello world")
+# -----------
+# res = 10 + 20
+# print(res)
+
+# --------------
+# a = 10 
+# b = 20
+# res = a + b 
+# print(res)
+# print("id:" , id(res))
+# print("type:", type(res))
+#----------------------
+"""
+this is a block comment
+"""
+# -----------------------
+sample_str = "this is string"
+print(dir(sample_str))
+
+'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill'
 
