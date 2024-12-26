bef42749e47b52218def5ece853ec21fc50a958d
Arunapindra <150082622+Arunapindra@users.noreply.github.com>
2024-12-25 21:29:40 -0600

    python

--- conditons.py
+++ conditons.py
@@ -5,3 +5,13 @@ if num1 <= 100:
     print("Number is less than or equal to 100")
 else:
     print(" No is more than 100")
+
+
+# if True:
+#     print("I am evaluated")
+
+# Anything that is non-zero is considered as true
+if "false":
+    print("I am evaluated")
+else:
+    print("Zero")
